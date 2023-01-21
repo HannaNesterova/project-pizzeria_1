beea080d9ee0b57a9a43ff6b861eac8ca2c4a181
Hanna Nesterova <nesterova.hanna@gmail.com>
2023-01-21 21:45:34 +0100

    Change initMenu, add for in for all productc

--- src/js/script.js
+++ src/js/script.js
@@ -59,8 +59,15 @@
   }
   const app = {
     initMenu : function() {
-      const testProduct = new Product();
-      console.log('testProduct:', testProduct)
+      const thisApp = this;
+
+      console.log('thisApp.data:', thisApp.data);
+      for(let productData in thisApp.data.products){
+        new Product(productData, thisApp.data.products[productData]);
+      }
+      // const testProduct = new Product();
+      // console.log('testProduct:', testProduct)
+   
     },
     initData: function(){
       const thisApp = this;
@@ -74,6 +81,7 @@
       console.log('settings:', settings);
       console.log('templates:', templates);
 
+      thisApp.initData();
       thisApp.initMenu();
     },
   };
