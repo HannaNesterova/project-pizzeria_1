162ac442e023e95095b402563e8d2b97713972bd
Hanna Nesterova <nesterova.hanna@gmail.com>
2023-01-28 12:56:20 +0100

    Call  thisProduct.addToCart() in the Product

--- src/js/script.js
+++ src/js/script.js
@@ -170,6 +170,7 @@
       thisProduct.cartButton.addEventListener('click', function(event){
         event.preventDefault();
         thisProduct.processOrder();
+        thisProduct.addToCart();
         
       });
 
