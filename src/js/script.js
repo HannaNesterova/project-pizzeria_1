366e4b6174e87a97b5d5a5c35e30703b5a0214d6
Hanna Nesterova <nesterova.hanna@gmail.com>
2023-02-06 00:20:11 +0100

    Create fetch for Cart.sendOrder

--- src/js/script.js
+++ src/js/script.js
@@ -424,6 +424,7 @@
       const url = settings.db.url + '/' + settings.db.orders;
 
      const payload = {
+      
         address: thisCart.dom.address.value,
         phone: thisCart.dom.phone.value,
         totalPrice: thisCart.dom.totalPrice.value,
@@ -433,6 +434,23 @@
         products: "tablica obecnych w koszyku produktów"
       }
       console.log('payload', payload);
+      for(let prod of thisCart.products) {
+        payload.products.push(prod.getData());
+      }
+      const options = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
+      };
+      
+      fetch(url, options)
+        .then(function(response){
+          return response.json();
+        }).then(function(parsedResponse){
+          console.log('parsedResponse', parsedResponse);
+        });
     }
   }
 
