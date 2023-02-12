029feb0a1675fad9c65abd81a86870beea7b3a7e
Hanna Nesterova <nesterova.hanna@gmail.com>
2023-02-12 17:51:33 +0100

    Cleane the files

--- src/js/app.js
+++ src/js/app.js
@@ -1,47 +1,45 @@
 const app = {
-    initMenu: function () {
-      const thisApp = this;
+  initMenu: function () {
+    const thisApp = this;
 
-      for (let productData in thisApp.data.products) {
-        new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
-      }
-    },
-    initData: function () {
-      const thisApp = this;
-      thisApp.data = {};
-      const url = settings.db.url + '/' + settings.db.products;
+    for (let productData in thisApp.data.products) {
+      new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
+    }
+  },
+  initData: function () {
+    const thisApp = this;
+    thisApp.data = {};
+    const url = settings.db.url + '/' + settings.db.products;
 
-      fetch(url)
-        .then(function(rawResponse){ //what does it mean?
-          return rawResponse.json();
-        })
-        .then(function(parsedResponse){
-          console.log(parsedResponse);
-          //save parsedResponse as thisApp.data.products;
-          thisApp.data.products = parsedResponse; 
+    fetch(url)
+      .then(function(rawResponse){ //what does it mean?
+        return rawResponse.json();
+      })
+      .then(function(parsedResponse){
+        console.log(parsedResponse);
+        //save parsedResponse as thisApp.data.products;
+        thisApp.data.products = parsedResponse; 
 
-          //execute initMenu method
-          thisApp.initMenu();
-        });
-      console.log('thisApp.data', JSON.stringify(thisApp.data));
-    },
-    initCart: function () {
-      const thisApp = this;
-      const cartElem = document.querySelector(select.containerOf.cart);
-      thisApp.cart = new Cart(cartElem);
-    },
-    init: function(){
-      const thisApp = this;
+        //execute initMenu method
+        thisApp.initMenu();
+      });
+    console.log('thisApp.data', JSON.stringify(thisApp.data));
+  },
+  initCart: function () {
+    const thisApp = this;
+    const cartElem = document.querySelector(select.containerOf.cart);
+    thisApp.cart = new Cart(cartElem);
+  },
+  init: function(){
+    const thisApp = this;
       
-      thisApp.initData();
-      //thisApp.initMenu();
-      thisApp.initCart();
-      
-      
-    },
-  };
+    thisApp.initData();
+    //thisApp.initMenu();
+    thisApp.initCart();
+  },
+};
   
-  app.init();
+app.init();
 
 
 
--- src/js/components/CartProduct.js
+++ src/js/components/CartProduct.js
@@ -1,84 +1,84 @@
 class CartProduct {
-    constructor(menuProduct, element, productList) {
-    //add thirt argument productList, but why?
+  constructor(menuProduct, element, productList) {
+  //add thirt argument productList, but why?
 
-      const thisCartProduct = this;
-      //зберегти в (thisCartProduct)) ньому всі властивості з menuProduct.
-      thisCartProduct.id = menuProduct.id;
-      thisCartProduct.productList = productList;
-      thisCartProduct.name = menuProduct.name;
-      thisCartProduct.amount = menuProduct.amount;
-      thisCartProduct.priceSingle = menuProduct.priceSingle;
-      thisCartProduct.price = menuProduct.price;
-      thisCartProduct.params = menuProduct.params;
+    const thisCartProduct = this;
+    //зберегти в (thisCartProduct)) ньому всі властивості з menuProduct.
+    thisCartProduct.id = menuProduct.id;
+    thisCartProduct.productList = productList;
+    thisCartProduct.name = menuProduct.name;
+    thisCartProduct.amount = menuProduct.amount;
+    thisCartProduct.priceSingle = menuProduct.priceSingle;
+    thisCartProduct.price = menuProduct.price;
+    thisCartProduct.params = menuProduct.params;
 
-      thisCartProduct.remove = this.remove; // посилається на функцію remove  from class CartProduct
-      thisCartProduct.edit = menuProduct.edit;
-      thisCartProduct.getElements(element);
-      thisCartProduct.initAmountWidget();
-      thisCartProduct.initActions();
-    }
-    getElements(element) {
-
-      const thisCartProduct = this;
-      thisCartProduct.dom ={};
-      thisCartProduct.dom.wrapper = element;
-      thisCartProduct.dom.amountWidget = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.amountWidget);
-      thisCartProduct.dom.price = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.price);
-      thisCartProduct.dom.edit = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.edit);
-      thisCartProduct.dom.remove  = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.remove);
-      thisCartProduct.dom.remove = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.remove);
-    }
-
-
-    initAmountWidget() {
-      const thisCartProduct = this;
-
-
-      thisCartProduct.amountWidget = new AmountWidget(thisCartProduct.dom.amountWidget);
-      thisCartProduct.dom.amountWidget.addEventListener('updated', function () {
-
-        thisCartProduct.amount = thisCartProduct.amountWidget.value;
-        thisCartProduct.price = thisCartProduct.amount * thisCartProduct.priceSingle;
-        thisCartProduct.dom.price.innerHTML = thisCartProduct.price;
-      });
-    }
-
-    remove(){
-      const thisCartProduct = this;
-      console.log(thisCartProduct);
-
-      const event = new CustomEvent('remove' ,{
-        bubbles: true,
-        detail: {
-          cartProduct: thisCartProduct,
-        },
-      });
-      thisCartProduct.dom.wrapper.dispatchEvent(event);
-    }
-    initActions(){
-      const thisCartProduct = this;
-
-      thisCartProduct.dom.edit.addEventListener('click', function(e){
-        e.preventDefault();
-      });
-      thisCartProduct.dom.remove.addEventListener('click', function(e){
-        e.preventDefault(e);
-        thisCartProduct.remove(event.detail.cartProduct);
-        console.log(thisCartProduct.dom.remove);
-      });
-    }
-    getData(){
-      const thisCartProduct = this;
-
-      const readyProductForServer = {
-        id: thisCartProduct.id,
-        amount : thisCartProduct.amount,
-        price : thisCartProduct.price,
-        params : thisCartProduct.params,
-        name : thisCartProduct.name,
-        priceSingle : thisCartProduct.priceSingle,
-      };
-      return readyProductForServer; //чому ми пишемо тут реторн а в sendOrde не писали?
-    }
+    thisCartProduct.remove = this.remove; // посилається на функцію remove  from class CartProduct
+    thisCartProduct.edit = menuProduct.edit;
+    thisCartProduct.getElements(element);
+    thisCartProduct.initAmountWidget();
+    thisCartProduct.initActions();
   }
+  getElements(element) {
+
+    const thisCartProduct = this;
+    thisCartProduct.dom ={};
+    thisCartProduct.dom.wrapper = element;
+    thisCartProduct.dom.amountWidget = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.amountWidget);
+    thisCartProduct.dom.price = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.price);
+    thisCartProduct.dom.edit = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.edit);
+    thisCartProduct.dom.remove  = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.remove);
+    thisCartProduct.dom.remove = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.remove);
+  }
+
+
+  initAmountWidget() {
+    const thisCartProduct = this;
+
+
+    thisCartProduct.amountWidget = new AmountWidget(thisCartProduct.dom.amountWidget);
+    thisCartProduct.dom.amountWidget.addEventListener('updated', function () {
+
+      thisCartProduct.amount = thisCartProduct.amountWidget.value;
+      thisCartProduct.price = thisCartProduct.amount * thisCartProduct.priceSingle;
+      thisCartProduct.dom.price.innerHTML = thisCartProduct.price;
+    });
+  }
+
+  remove(){
+    const thisCartProduct = this;
+    console.log(thisCartProduct);
+
+    const event = new CustomEvent('remove' ,{
+      bubbles: true,
+      detail: {
+        cartProduct: thisCartProduct,
+      },
+    });
+    thisCartProduct.dom.wrapper.dispatchEvent(event);
+  }
+  initActions(){
+    const thisCartProduct = this;
+
+    thisCartProduct.dom.edit.addEventListener('click', function(e){
+      e.preventDefault();
+    });
+    thisCartProduct.dom.remove.addEventListener('click', function(e){
+      e.preventDefault(e);
+      thisCartProduct.remove(event.detail.cartProduct);
+      console.log(thisCartProduct.dom.remove);
+    });
+  }
+  getData(){
+    const thisCartProduct = this;
+
+    const readyProductForServer = {
+      id: thisCartProduct.id,
+      amount : thisCartProduct.amount,
+      price : thisCartProduct.price,
+      params : thisCartProduct.params,
+      name : thisCartProduct.name,
+      priceSingle : thisCartProduct.priceSingle,
+    };
+    return readyProductForServer; //чому ми пишемо тут реторн а в sendOrde не писали?
+  }
+}
