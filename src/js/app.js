766ac92a5e45e305ada44c081c6a37312bc0062d
Hanna Nesterova <nesterova.hanna@gmail.com>
2023-02-13 19:19:08 +0100

    Create intitPages and function activatePage

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,46 @@
-import { settings, select } from './setting.js';
+import { settings, select, classNames } from './setting.js';
 import Product  from './components/Product.js';
 import Cart from './components/Cart.js';
 
 
 const app = {
+  initPages: function (){
+const thisApp = this;
+
+thisApp.pages = document.querySelector(select.containerOf.pages).children;
+thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+thisApp.activatePage(thisApp.pages[0].id);
+for(let link of thisApp.navLinks){
+  link.addEventListener('click', function(event){
+    const clickedElement = this;
+    event.preventDefault();
+
+    //get ID from href attribute
+    const id = clickedElement.getAttribute('href').replace('#', '');
+    //run thisApp.activatePage with this ID
+    thisApp.activatePage(id);
+  });
+}
+
+},
+
+activatePage: function(pageId){
+const thisApp = this;
+
+//add class active to matching pages, remove from non-matching
+for(let page of thisApp.pages){
+  page.classList.toggle(classNames.pages.active, page.id == pageId);
+}
+
+//add class active to matching links, remove from non-matching
+for(let link of thisApp.navLinks){
+  link.classList.toggle(
+    classNames.nav.active, 
+    link.getAttribute('href') == '#' + pageId
+    );
+}
+},
   initMenu: function () {
     const thisApp = this;
 
@@ -43,7 +80,8 @@ const app = {
   },
   init: function(){
     const thisApp = this;
-      
+     
+    thisApp.initPages();
     thisApp.initData();
     //thisApp.initMenu();
     thisApp.initCart();
