02d8006247723c54396a75c6f4059a1e8414f59c
Hanna Nesterova <nesterova.hanna@gmail.com>
2023-02-14 19:58:09 +0100

    listeners to initAmountWidget

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,36 +1,36 @@
 import {select} from './setting.js';
 import AmountWidget from './AmountWidget.js'; // is not defined
 
+
 class Booking {
 
     constructor(element) {
       const thisBooking = this;
       thisBooking.render(element);
-    }
+      thisBooking.initAmountWidget();
+        }
 
       render (element) {
         const thisBooking = this;
         const generatedHTML = templates.bookingWidget();
+
         thisBooking.dom = {};
         thisBooking.dom.wrapper = element;
         thisBooking.dom.wrapper.innerHTML = generatedHTML;
-        thisBooking.initWidgets ();
+        
 
         thisBooking.dom.peopleAmount = document.querySelector(select.cart.booking.peopleAmount);
         thisBooking.dom.hoursAmount=document.querySelector(select.cart.booking.hoursAmount);
       }
-        initAmountWidget() {
+      initAmountWidget() {
          const thisBooking = this;
-    
-    
-            //thisBooking.amountWidget = new AmountWidget(thisCartProduct.dom.amountWidget);
-            //thisBooking.dom.amountWidget.addEventListener('updated', function () {
-    
-            thisBooking.amountPeople = thisBooking.dom.peopleAmount.value;
-            thisBooking.hoursAmount = thisBooking.dom.hoursAmount.value;
-    //       thisCartProduct.dom.price.innerHTML = thisCartProduct.price;
-    //     });
-    //   }
+        
+         thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
+         thisBooking.dom.peopleAmount.addEventListener('update', function(){});
+
+         thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
+         thisBooking.dom.hoursAmount.addEventListener('update', function(){});
+
 }
 }
 
