29fea93ba3498115ffc68b60e3203c16a40e8a51
Hanna Nesterova <nesterova.hanna@gmail.com>
2023-02-17 15:37:15 +0100

    Start work with getData

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,5 +1,5 @@
 
-import { select, templates } from '../setting.js';
+import { select, templates, settings } from '../setting.js';
 import AmountWidget from './AmountWidget.js';
 import DatePicker from './DatePicker.js';
 import HourPicker from './HourPicker.js';
@@ -9,8 +9,32 @@ class Booking {
     const thisBooking = this;
     thisBooking.render(element);
     thisBooking.initWidgets();
-
+    thisBooking.getData();
   }
+getData(){
+  const thisBooking = this; 
+
+  const params = {
+    booking: [
+
+    ],
+    eventsCurrent: [
+
+    ],
+    eventsRepeat : [
+
+    ],
+};
+console.log('getData params', params)
+
+  const urls = {
+    booking:        settings.db.url + '/' + settings.db.booking + '?' + params.booking.join('&'),
+    eventsCurrent:  settings.db.url + '/' + settings.db.event   + '?' + params.eventsCurrent.join('&'),
+    eventsRepeat:   settings.db.url + '/' + settings.db.event   + '?' + params.eventsRepeat.join('&'),
+  }
+
+}
+
   render(element) {
     const thisBooking = this;
 
