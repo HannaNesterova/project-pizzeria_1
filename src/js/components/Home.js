b5c74f8f4048e367bac4bc0b8570fe4dc8608287
Hanna Nesterova <nesterova.hanna@gmail.com>
2023-02-22 21:37:03 +0100

    Try to listen a HOME

--- src/js/components/Home.js
+++ src/js/components/Home.js
@@ -1,4 +1,4 @@
-import {templates, select, classNames} from '../settings.js';
+import {templates} from '../settings.js';
 import utils from '../utils.js';
 
 class Home {
@@ -18,48 +18,27 @@ class Home {
     const element = utils.createDOMFromHTML(generatedHTML);
     thisHome.dom.wrapper.appendChild(element);
 
-    thisHome.dom.order = document.querySelector('#order');
-    thisHome.dom.btnOrder = document.querySelector('.home_container-item');
-    console.log(thisHome.dom.order);
-    console.log(thisHome.dom.btnOrder);
-
-  }
-  render(){
-  const thisHome = this;
     const order = document.querySelector('a[href="#order"]');
     console.log('order', order);
 
+    thisHome.btnOrder = document.querySelector('item-one');
+
     const booking = document.querySelector('a[href="#booking"]');
     console.log('booking', booking);
-  }
-   activePage() {
-     const thisHome = this;
-     if (thisHome.order) {
-       thisHome.order.addEventListener('update', function() {
-         thisHome.dom.order.classList.add('active');
-         console.log('Button clicked!');
-       });
-     }
-   }
-  
-/*activatePage (pageId) {
-    const thisHome = this;
-    thisHome.pages = document.querySelectorAll('pages');
-    thisHome.navLinks = document.querySelectorAll(select.nav.links);
-    console.log(thisHome.pages);
 
-    /*add class Active to matching pages, remove class Active from non-matching pages */
-    // for (let page of thisHome.pages) {
-    //   page.classList.toggle(classNames.pages.active, page.id == pageId);
-    // }
-    // for (let link of thisHome.navLinks) {
-    //   link.classList.toggle(
-    //     classNames.nav.active,
-    //     link.getAttribute('href') == '#' + pageId
-    //   );
-    // }
-    /*add class Active to matching links, remove class Active from non-matching links */
-  //}
+  }
+
+   activePage() {
+    const thisHome = this;
+    
+    if (thisHome.btnOrder) {
+        thisHome.btnOrder.addEventListener('click', function() {
+          // Add the "active" class to the "order" element
+          order.classList.add('active');
+          console.log('Button clicked!');
+        });
+     }
+  }
 }
 
 export default Home;
