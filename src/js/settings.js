47adae3852e3a6e764dbdadf74356a58fd6f5b19
Hanna Nesterova <nesterova.hanna@gmail.com>
2023-02-23 19:39:06 +0100

    url

--- src/js/settings.js
+++ src/js/settings.js
@@ -123,10 +123,11 @@ export const settings = {
     tableIdAttribute: 'data-table',
   },
   db: {
-    url:
-      '//' +
-      window.location.hostname +
-      (window.location.hostname == 'localhost' ? ':3131' : ''),
+    url: '//' + window.location.hostname + (window.location.hostname=='localhost' ? ':3131' : ''),
+    // url:
+    //   '//' +
+    //   window.location.hostname +
+    //   (window.location.hostname == 'localhost' ? ':3131' : ''),
     products: 'products',
     orders: 'orders',
     product: 'product',
